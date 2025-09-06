dc73fa13030fbbd75e869e155ec0651e9eb0e605
atoll6 <arda.tolan@gmail.com>
2025-09-06 17:26:34 +0200

    Switched to box64 with box32 for emulation, building it during image creation. Removed pause funciton.

--- Dockerfile
+++ Dockerfile
@@ -1,39 +1,22 @@
-FROM debian@sha256:914261d1145adceace843aa24efcc339f5ce4f570a192dad4bd2d31241fbeb7d
-
+FROM debian:bookworm-slim
 
 #Install Prereqs
-RUN chmod 1777 /tmp \
-&&  dpkg --add-architecture armhf \
-&&  apt update -y \
-&&  apt upgrade -y \
-&&  apt install -y  apt-utils knockd unzip wget curl gpg systemd \
-                    libc6 libc6:armhf libatomic1 libatomic1:armhf libpulse-dev libpulse-dev:armhf libpulse0 libpulse0:armhf libmonoboehm-2.0-1 libmonoboehm-2.0-1:armhf
+RUN dpkg --add-architecture armhf \
+&&  apt-get update -y \
+&&  apt-get upgrade -y \
+&&  apt-get install -y wget gpg git tar build-essential cmake python3 libc6:armhf
 
-#Install Box86-Repo
-
-RUN wget --inet4-only https://itai-nelken.github.io/weekly-box86-debs/debian/box86.list -O /etc/apt/sources.list.d/box86.list \
-&&  wget --inet4-only -qO- https://itai-nelken.github.io/weekly-box86-debs/debian/KEY.gpg | gpg --dearmor -o /etc/apt/trusted.gpg.d/box86-debs-archive-keyring.gpg \
-&&  apt update \
-&&  apt install box86:armhf -y
-
-
-#Install Box64-Repo
-RUN wget --inet4-only https://ryanfortner.github.io/box64-debs/box64.list -O /etc/apt/sources.list.d/box64.list \
-&&  wget --inet4-only -qO- https://ryanfortner.github.io/box64-debs/KEY.gpg | gpg --dearmor -o /etc/apt/trusted.gpg.d/box64-debs-archive-keyring.gpg \
-&&  apt update \
-&&  apt install -y box64-rpi4arm64
-
-#Configure box64 for Valheim-Server
-RUN echo "" >> /etc/box64.box64rc \
-&&  echo "[valheim_server.x86_64]" >> /etc/box64.box64rc \
-&&  echo "BOX64_DYNAREC_STRONGMEM=2" >> /etc/box64.box64rc \
-&&  echo "BOX64_DYNAREC_BLEEDING_EDGE=0" >> /etc/box64.box64rc \
-&&  echo "BOX64_DYNAREC_BIGBLOCK=3" >> /etc/box64.box64rc
+#Build box64 with box32
+RUN git clone https://github.com/ptitSeb/box64 \
+&&  cd box64 \
+&&  mkdir build; cd build; cmake .. -D RPI5ARM64=1 -D ARM_DYNAREC=ON -D CMAKE_BUILD_TYPE=RelWithDebInfo -D BOX32=ON -D BOX32_BINFMT=ON \
+&&  make -j4 \
+&&  make install
 
 # Install Steam
 RUN mkdir   /steamcmd \
 &&  cd      /steamcmd \
-&&  curl -sqL "https://steamcdn-a.akamaihd.net/client/installer/steamcmd_linux.tar.gz" | tar zxvf -
+&&  wget -qO- "https://steamcdn-a.akamaihd.net/client/installer/steamcmd_linux.tar.gz" | tar zxvf -
 
 # Install required library and scripts
 COPY ./add /scripts/
@@ -46,11 +29,8 @@ ENV SERVER_NAME=Raspiheim \
     SAVE_DIR=/data \
     PUBLIC=disabled \
     UPDATE=enabled \
-    PAUSE=disabled \
     SAVE_INTERVAL=1800 \
-    CROSSPLAY=disabled \
-    BOX86=default \
-    BOX64=default
+    CROSSPLAY=enabled
 
 EXPOSE 2456/udp 2457/udp
 
--- add/valheim.sh
+++ add/valheim.sh
@@ -4,57 +4,22 @@ cp /etc/resolv.conf /
 sed -i s/"nameserver ".*/"nameserver 1.1.1.1"/g /resolv.conf
 cp /resolv.conf /etc/resolv.conf
 
-#Manage Box86/64 version
-if [ "$BOX86" != "default" ];
-then	installed="$(apt-cache policy box86-rpi4arm64 | grep Installed | awk '{print $2}')"
-		wanted="$(apt-cache policy box86-rpi4arm64 | grep 500 | grep "$BOX86" | awk 'NR==1 {print $2}')"
-		latest="$(apt-cache policy box86-rpi4arm64 | grep Candidate | awk '{print $2}')"
-
-	if [ "$BOX86" = "latest" ] && [ "$installed" != "$latest" ];
-			then apt-get purge box86-rpi4arm64 -y && apt-get install box86-rpi4arm64=$latest;
-	elif [ "$BOX86" = "wanted" ] && [ "$installed" != "$wanted" ];
-			then apt-get purge box86-rpi4arm64 -y && apt-get install box86-rpi4arm64=$wanted;
-	fi;
-fi;
-
-if [ "$BOX64" != "default" ];
-then	installed="$(apt-cache policy box64-rpi4arm64 | grep Installed | awk '{print $2}')"
-		wanted="$(apt-cache policy box64-rpi4arm64 | grep 500 | grep "$BOX64" | awk 'NR==1 {print $2}')"
-		latest="$(apt-cache policy box64-rpi4arm64 | grep Candidate | awk '{print $2}')"
-
-	if [ "$BOX64" = "latest" ] && [ "$installed" != "$latest" ];
-			then apt-get purge box64-rpi4arm64 -y && apt-get install box64-rpi4arm64=$latest;
-	elif [ "$BOX64" = "wanted" ] && [ "$installed" != "$wanted" ];
-			then apt-get purge box64-rpi4arm64 -y && apt-get install box64-rpi4arm64=$wanted;
-	fi;
-fi;
-
 # Upgrade Valheim to latest Version
 if [ ! -f /valheim/start_server.sh ] || [ $UPDATE = enabled ] || [ $UPDATE = 1 ]; then
 cd /steamcmd
 echo "Updating the server..."
 export LD_LIBRARY_PATH="/steamcmd/linux32:$LD_LIBRARY_PATH" && \
-box86 ./linux32/steamcmd +login anonymous +quit >/dev/null;
+box64 ./linux32/steamcmd +login anonymous +quit >/dev/null;
 export LD_LIBRARY_PATH="/steamcmd/linux32:$LD_LIBRARY_PATH" && \
-box86 ./linux32/steamcmd +login anonymous +quit >/dev/null;
+box64 ./linux32/steamcmd +login anonymous +quit >/dev/null;
 export LD_LIBRARY_PATH="/steamcmd/linux32:$LD_LIBRARY_PATH" && \
-box86 ./linux32/steamcmd +@sSteamCmdForcePlatformType linux +force_install_dir /valheim +login anonymous +app_update 896660 validate +quit;
+box64 ./linux32/steamcmd +@sSteamCmdForcePlatformType linux +force_install_dir /valheim +login anonymous +app_update 896660 validate +quit;
 fi;
 
 # Manage Persistency
 cp -f /scripts/start_server.sh.tpl	/valheim/start_server.sh
 
 cd /valheim
-
-#Pause Option
-if [ $PAUSE = enabled ] || [ $PAUSE = 1 ]; then
-mkdir -p /data/logs
-cp /scripts/knockd.conf /etc/knockd.conf
-sed -i s.START_KNOCKD=0.START_KNOCKD=1.g /etc/default/knockd
-service knockd restart
-chmod +x /scripts/pause.sh
-(/scripts/pause.sh)&
-fi
 
 # Start Server
 chmod +x ./start_server.sh
